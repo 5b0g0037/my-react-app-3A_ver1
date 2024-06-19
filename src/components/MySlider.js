ad766ab1f7a9a131bf38a9274720a5b3f011f85c
5b0g0037 <5b0g0037@stust.edu.tw>
2024-06-19 13:01:24 +0800

    2

--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
-function MySlider({onChange}) {
+function MySlider({label, onChange}) {
   const [value, setValue] = useState(128);
-  const handleChange = (e) => {
+  const handleSliderChange = (e) => {
     setValue(e.target.value);
     if (onChange){
       onChange(e.target.value);
     }
   };
+
   return (
     <div>
+      <span>{label}</span>
       <input
         type="range"
-        width = "200"
+        width="400"
         min="0"
         max="255"
         value={value}
-        onChange={handleChange}
+        onChange={handleSliderChange}
       />
       <span>{value}</span>
     </div>
   );
 }
-
 export default MySlider;
